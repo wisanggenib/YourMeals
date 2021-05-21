1ccb476078b1a92ef3358bb08a5ceca8f59fdd65
wisanggenib <bagus.wisanggeni4897@gmail.com>
2021-05-21 17:09:42 +0700

    list category

--- src/container/category/category.container.js
+++ src/container/category/category.container.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import {View, Text, Button, TextInput} from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  FlatList,
+  TouchableWithoutFeedback,
+  TouchableNativeFeedback,
+} from 'react-native';
+
+import {CATEGORIES} from '../../datas/data';
 
 import {Icons} from '../../components';
 
 import Styles from './category.style';
 
+const renderItem = ({id, color, title}) => {
+  return (
+    <View style={Styles.itemsContainer(color)}>
+      <TouchableNativeFeedback>
+        <View style={Styles.items}>
+          <Text style={Styles.itemsTitle}>{title}</Text>
+        </View>
+      </TouchableNativeFeedback>
+    </View>
+  );
+};
+
 const Category = props => {
   return (
     <View style={Styles.container}>
-      <Text>
-        Category Screen <Icons icon="favorite" />
-      </Text>
-      <Button
-        title="Meals"
-        onPress={() => props.navigation.navigate('Meals')}
+      <FlatList
+        numColumns={2}
+        data={CATEGORIES}
+        contentContainerStyle={Styles.listContainer}
+        renderItem={({item}) => renderItem(item)}
       />
     </View>
   );
--- src/container/category/category.style.js
+++ src/container/category/category.style.js
@@ -4,4 +4,30 @@ export default StyleSheet.create({
   container: {
     flex: 1,
   },
+  itemsContainer: color => ({
+    margin: 10,
+    height: 150,
+    width: '45%',
+    borderRadius: 25,
+    overflow: 'hidden',
+    backgroundColor: color,
+  }),
+  items: {
+    width: '100%',
+    height: '100%',
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+  },
+  listContainer: {
+    margin: 8,
+    paddingBottom: 20,
+  },
+  itemsTitle: {
+    fontSize: 24,
+    textAlign: 'right',
+    color: 'white',
+    fontWeight: 'bold',
+  },
 });
